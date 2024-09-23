e00536526d666203da3384a28e2a6f9ed938708f
Kenneth Lee <kennethlee@fortinet.com>
2024-09-23 18:41:11 -0400

    better spacing

--- src/routes/Toolbar.svelte
+++ src/routes/Toolbar.svelte
@@ -3,36 +3,40 @@
 </script>
 
 <article>
-  <label><input type="radio" bind:group={tool} value="path"> point</label>
-  <label><input type="radio" bind:group={tool} value="line"> line</label>
-  <label><input type="radio" bind:group={tool} value="shape"> shape</label>
-  <label><input type="radio" bind:group={tool} value="rect" disabled> rect</label>
-  <label><input type="radio" bind:group={tool} value="arc" disabled> circle</label>
+  <div>
+    <label><input type="radio" bind:group={tool} value="path"> point</label>
+    <label><input type="radio" bind:group={tool} value="line"> line</label>
+    <label><input type="radio" bind:group={tool} value="shape"> shape</label>
+    <label><input type="radio" bind:group={tool} value="rect" disabled> rect</label>
+    <label><input type="radio" bind:group={tool} value="arc" disabled> circle</label>
+    <input type="color" bind:value={color} />
+  </div>
   <vb />
-  <button onclick={onundo}>undo</button>
-  <button onclick={onredo}>redo</button>
-  <br />
-
-  <!-- <input type="color" bind:value={color} /> -->
+  <div>
+    <button onclick={onundo}>undo</button>
+    <button onclick={onredo}>redo</button>
+  </div>
 </article>
 
 <style>
   article {
-    /* position: absolute; */
     display: flex;
-    height: 48px;
+    height: 32px;
     left: calc(50% - 100px);
-    /* bottom: 0; */
+    line-height: 32px;
 
     background: white;
-
     border: 1px solid grey;
     box-shadow: inset 0 0 3px rgba(24, 87, 96,.5);
+    font-size: smaller;
   }
 
   label {
     text-transform: capitalize;
     white-space: nowrap;
+  }
+  input {
+    vertical-align: sub;
   }
 
   vb {
